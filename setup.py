9a833cdd2c9e6a8aa5373f7a5afda2f7842d116f
asparagusbeef <jonathanoren98@gmail.com>
2023-07-24 23:59:39 -0400

    patch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easyID3',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='Implementation of the original ID3 algorithm in Python.',
     author='Jonathan',
